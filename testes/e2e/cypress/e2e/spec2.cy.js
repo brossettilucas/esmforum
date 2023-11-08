439b1f270cb88c30295d4d637fb2ca4fe230df63
Lucas Braz <79875594+brossettilucas@users.noreply.github.com>
2023-11-08 16:37:17 -0300

    Update spec2.cy.js

--- testes/e2e/cypress/e2e/spec2.cy.js
+++ testes/e2e/cypress/e2e/spec2.cy.js
@@ -1,6 +1,6 @@
 describe('Segundo teste end-to-end', () => {
     it('Cadastra uma resposta e verifica se ela é listada', () => {
-      cy.visit('localhost:3000');
+      cy.visit('localhost:3000/resposta/1');
       
       cy.get('#textarea-resposta').type('6');
       cy.get('#btn-resposta').click();
